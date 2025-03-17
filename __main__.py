dae9d7ac825874d828aa9124c8cc7d5f9b6a198b
Gabrielle Ribeiro <33001620+Gabrielle-Ribeiro@users.noreply.github.com>
2025-03-17 18:08:14 -0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,22 +1,13 @@
-import pulumi
-import pulumi_docker as docker
+import pulumi 
+import pulumi_docker as docker 
 
-network = docker.Network("escola-network")
-
-def criar_container(nome, imagem, porta_interna, porta_externa):
-    return docker.Container(
-        nome,
-        image=imagem,
-        ports=[
-            {
-                "internal": porta_interna,
-                "external": porta_externa,
-            }
-        ],
-        networks_advanced=[{
-            "name": network.name, 
-        }]
-    )
-
-backend = criar_container("api-container", "gabiribeiro/api-escola:latest", 8000, 8000)
-frontend = criar_container("web-container", "gabiribeiro/web-escola:latest", 80, 8042)
+frontend = docker.Container( 
+    "web-container", 
+    image="gabiribeiro/web-escola:latest", 
+    ports=[ 
+        { 
+            "internal": 80,       
+            "external": 8042,    
+        } 
+    ] 
+) 
